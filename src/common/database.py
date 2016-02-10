593fd9c1f2d2d0b68b45ca16e6b7c682e6d2afd4
jslvtr <jslvtr@gmail.com>
2016-02-10 12:33:04 +0000

    Adding the ability to read environment variable in database.

--- src/common/database.py
+++ src/common/database.py
@@ -1,10 +1,12 @@
+import os
+
 import pymongo
 
 __author__ = 'jslvtr'
 
 
 class Database(object):
-    URI = "mongodb://127.0.0.1:27017"
+    URI = os.environ.get("MONGOLAB_URI")
     DATABASE = None
 
     @staticmethod
