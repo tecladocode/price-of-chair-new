71b5ff34ce4cfc38bd36b419c10a14558e543c9e
jslvtr <jslvtr@gmail.com>
2016-02-10 12:13:40 +0000

    Adding required Heroku files, and reading from environment vars instead of hardcoded API values.

--- src/models/alerts/constants.py
+++ src/models/alerts/constants.py
@@ -1,8 +1,10 @@
+import os
+
 __author__ = 'jslvtr'
 
 
-URL = "https://api.mailgun.net/v3/<>.mailgun.org/messages"
-API_KEY = "key-<>"
-FROM = "Mailgun Sandbox <postmaster@<>.mailgun.org>"
+URL = os.environ.get('MAILGUN_URL')
+API_KEY = os.environ.get('MAILGUN_API_KEY')
+FROM = os.environ.get('MAILGUN_FROM')
 ALERT_TIMEOUT = 10
 COLLECTION = "alerts"
